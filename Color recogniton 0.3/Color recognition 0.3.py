cc388ef2a6c5a6571a17541048853eb484a1c0bb
Abelardo Karpov <137881400+Abelardo-Karpov@users.noreply.github.com>
2024-12-20 01:05:37 -0800

    Add files via upload
    more simple

---
+++ Color recogniton 0.3/Color recognition 0.3.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+from Detect_Color_03 import Detect_Color
+
+
+
+webcam = cv2.VideoCapture(2)
+
+while (1):
+	## Asigna variable a los frames
+	_, frame = webcam.read()
+
+	red = Detect_Color(frame, [170, 130, 100], [180, 255, 255], 800, "Red", (0,0,255))
+	blue = Detect_Color(frame, [110, 180, 0], [116, 255, 255], 800, "Blue", (255,0,0))
+	green = Detect_Color(frame, [40, 150, 20], [70, 255, 255], 800, "Green", (0,255,0))
+	orange = Detect_Color(frame, [2.5, 140, 100], [13, 255, 255], 800, "Orange", (6, 100, 255))
+	yellow = Detect_Color(frame, [17.5, 100, 100], [30, 255, 255], 800, "Yellow", (6, 255, 255))
+	cyan = Detect_Color(frame, [95, 112, 120], [108, 255, 255], 800, "Cyan", (255, 255, 0))
+	purple = Detect_Color(frame, [118, 100, 100], [130, 255, 255], 800, "Purple", (128, 0, 128))
+	black = Detect_Color(frame, [0, 0.2, 0.2], [0, 32, 32], 800, "Black", (0, 2, 2))
+	pink = Detect_Color(frame, [135, 100, 100], [160, 255, 255], 800, "Pink", (255, 0, 255))
+	gray = Detect_Color(frame, [0, 0, 0], [80, 80, 80], 800, "Gray", (128, 128, 128))
+
+	# Iniciar programa
+	## muestra el programa con el nombre Color Detection y las frames modificadas
+	cv2.imshow("Color Detection", frame)
+	if cv2.waitKey(10) & 0xFF == ord('q'):
+		webcam.release()
+		cv2.destroyAllWindows()
+		break
---
+++ Color recogniton 0.3/Detect_Color_03.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+class Detect_Color:
+	def __init__(self, frame, range_lower, range_upper, area, name, bgr):
+		self.frame = frame 
+		self.hsvFrame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+		self.range_lower = np.array(range_lower, np.uint8)
+		self.range_upper = np.array(range_upper, np.uint8)
+		self.color_mask = cv2.inRange(self.hsvFrame, self.range_lower, self.range_upper)
+		self.kernel = np.ones((5, 5), "uint8")
+		self.area = area
+		self.name = name 
+		self.bgr = bgr
+		self.contours, self.hierarchy = cv2.findContours(self.color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		for pic, contour in enumerate(self.contours):
+			self.area = cv2.contourArea(contour)
+			if (self.area > 300):
+				#investigar que hace boundingrect
+				x, y, w, h = cv2.boundingRect(contour)
+				#imageframe pq es un visual que se mostrara ahi
+				self.frame = cv2.rectangle(self.frame, (x, y),
+										(x + w, y + h),
+										self.bgr, 2)
+				cv2.putText(self.frame, self.name, (x, y),
+							cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+							self.bgr)
